bfdb1ded21552e6e70f34215f58556b20af4c88d
KamranYsupov <kamrankek48@gmail.com>
2025-10-18 17:14:53 +0400

    init и RepositoryPhone

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,2 +1,12 @@
-__all__ = ()
+__all__ = (
+    "Organization",
+    "Phone",
+    "Activity",
+    "Building",
+)
+
+from app.repositories.activity import Activity
+from app.repositories.building import Building
+from app.repositories.organization import Organization, Phone
+
 
--- app/repositories/organization.py
+++ app/repositories/organization.py
@@ -1,7 +1,11 @@
-from app.db.models import Organization
+from app.db.models import Organization, Phone
 from app.repositories.base import RepositoryBase
 
 
 class RepositoryOrganization(RepositoryBase[Organization]):
     """Репозиторий модели Organization"""
+    pass
+
+class RepositoryPhone(RepositoryBase[Phone]):
+    """Репозиторий модели Phone"""
     pass
